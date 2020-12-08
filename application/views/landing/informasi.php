9a7c1d981d7fcf50b4521c69398ebc1f155d7bff
unknown <ondabuzz9@gmail.com>
2020-12-08 12:21:04 +0700

    fix show image info

--- application/views/landing/informasi.php
+++ application/views/landing/informasi.php
@@ -21,11 +21,17 @@
             </div>
             <?php foreach ($informasi as $info) :?>
             <div class="container-info mb-3">
-               <div class="info">
-                   <h5><?php echo $info['judul_info']?></h5>
-                    <h7><?php echo $info['isi_info']?></h7> 
+                <div class="info">
+                    <h5 class="mb-2"><?php echo $info['judul_info']?></h5>
+                    <?php if ($info['gbr_info'] == '') :?>
+                    <?php elseif($info['gbr_info'] >= 0) :?>
+                    <img src="<?php echo base_url() . 'assets/landing/img/info/'.$info['gbr_info'];?>" width="300"
+                        height="160" id="previmg" name="previmg">
+                    <br>
+                    <?php endif; ?>
+                    <h7 class="mb-2"><?php echo $info['isi_info']?></h7>
                     <p>Diposting pada : <?php echo $info['tgl_info']?></p>
-               </div>
+                </div>
             </div>
             <?php endforeach; ?>
         </div>
